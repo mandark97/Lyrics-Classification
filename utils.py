a9a77f958f4377ecfefff6c6fdd7d0cc00a3f8d2
Matei Melinte <matei.melinte@sparktech.ro>
2019-05-08 15:59:25 +0300

    add tpot tests

--- utils.py
+++ utils.py
@@ -9,13 +9,22 @@ class ResultLogger(object):
         self.log_type = log_type
         self.obj = obj
 
-    def score(self, X_test, y_test):
+    def score(self, X_test, y_test, save_to_csv=True):
         if self.log_type == self.PIPELINE:
             score = self.obj.score(X_test, y_test)
             print(score)
 
-            return {
+            data = {
                 "steps": self.obj.steps,
                 "score": score,
                 "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             }
+            if save_to_csv:
+                self.__to_csv(data)
+            else:
+                return data
+
+    def __to_csv(self, data):
+        with open("pipeline_results.csv", "a") as f:
+            dict_writer = csv.DictWriter(f, fieldnames=['timestamp', 'score', 'steps'])
+            dict_writer.writerow(data)
